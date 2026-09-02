baf1bbee5f6864109dcde073788034a5f7bcda93
acardosozz <liviasilvamora@gmail.com>
2026-09-02 09:25:07 -0300

    Update dados.js

--- api/dados.js
+++ api/dados.js
@@ -3,23 +3,27 @@ export default async function handler(req, res) {
   const SUPABASE_URL = process.env.SUPABASE_URL;
   const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY;
 
+  // RECEBE OS DADOS DO ESP32 E SALVA NO SUPABASE
   if (req.method === "POST") {
 
     const { turbidez, condutividade } = req.body;
 
-    const resposta = await fetch(`${SUPABASE_URL}/rest/v1/leituras`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "apikey": SUPABASE_SECRET_KEY,
-        "Authorization": `Bearer ${SUPABASE_SECRET_KEY}`,
-        "Prefer": "return=representation"
-      },
-      body: JSON.stringify({
-        turbidez,
-        condutividade
-      })
-    });
+    const resposta = await fetch(
+      `${SUPABASE_URL}/rest/v1/leituras`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "apikey": SUPABASE_SECRET_KEY,
+          "Authorization": `Bearer ${SUPABASE_SECRET_KEY}`,
+          "Prefer": "return=representation"
+        },
+        body: JSON.stringify({
+          turbidez,
+          condutividade
+        })
+      }
+    );
 
     const dados = await resposta.json();
 
@@ -33,16 +37,33 @@ export default async function handler(req, res) {
     return res.status(200).json({
       sucesso: true,
       mensagem: "Dados salvos no Supabase",
-      dados
+      dados: dados
     });
   }
 
+  // BUSCA A MEDIÇÃO MAIS RECENTE NO SUPABASE
   if (req.method === "GET") {
 
-    return res.status(200).json({
-      urlTeste: `${SUPABASE_URL}/rest/v1/leituras`
-    });
+    const resposta = await fetch(
+      `${SUPABASE_URL}/rest/v1/leituras?select=*&order=created_at.desc&limit=1`,
+      {
+        headers: {
+          "apikey": SUPABASE_SECRET_KEY,
+          "Authorization": `Bearer ${SUPABASE_SECRET_KEY}`
+        }
+      }
+    );
 
+    const dados = await resposta.json();
+
+    if (!resposta.ok) {
+      return res.status(500).json({
+        sucesso: false,
+        erro: dados
+      });
+    }
+
+    return res.status(200).json(dados[0] || {});
   }
 
   return res.status(405).json({
